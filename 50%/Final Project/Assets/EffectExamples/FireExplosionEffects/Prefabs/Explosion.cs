7b8e5b00f94cfd8cd9d491b5724c65ac2f1efb7f
gpolking <45469984+gpolking@users.noreply.github.com>
2018-12-11 16:48:37 -0500

    Update

---
+++ 50%/Final Project/Assets/EffectExamples/FireExplosionEffects/Prefabs/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rb;
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Explode"))
+        {
+            other.gameObject.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    
+}
---
+++ 50%/Final Project/Assets/MoveTo2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class MoveTo2 : MonoBehaviour {
+
+    public Transform[] goal;
+    int currentGoal;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+       agent = GetComponent<NavMeshAgent>();
+        agent.Warp();
+        GetTarget();
+
+    }
+    void Update()
+    {
+        if(Vector3.Distance(goal[currentGoal].position, transform.position) < 2f)
+        {
+            GetTarget();
+        }
+    }
+
+    void GetTarget()
+    {
+        currentGoal = Random.Range(0, goal.Length);
+        agent.destination = goal[currentGoal].position;
+    }
+}
